52829f9b4d0e6640c477f2a043481425ffd686e2
Ankit Bisht <anbisht@microsoft.com>
2018-05-16 19:05:16 +0530

    added better unit test examples

--- Service-Fabric-Client-Test-Proj-Demo/Service-Fabric-Client-Test-Proj-Demo/TestExamples/UnitTest1.cs
+++ Service-Fabric-Client-Test-Proj-Demo/Service-Fabric-Client-Test-Proj-Demo/TestExamples/UnitTest1.cs
@@ -9,51 +9,91 @@ using Service_Fabric_Test_Model.ServiceFabricTestClientLib;
 namespace DemoTestCasesNamespace
 {
     //->Functions should have no arguments.
-    //->Only functions having 'TestMethodServiceFabricClient' attribute inside class having 'TestClassServiceFabricClient' attribute will run respective test case on server. Any other scheme of assigning attributes will not run test cases on server side. However, 'TestMethodServiceFabricClient', 'TestClass' pair or 'TestMethod', 'TestClassServiceFabricClient' pair will run tests on client side and have behaviour equivalent of 'TestMethod', 'TestClass'.
-    
+    //->Only functions having 'TestMethodServiceFabricClient' attribute inside class having 'TestClassServiceFabricClient' attribute will run respective test case on server. Any other scheme of assigning attributes will not run test cases on server side.
+
     //[TestClass]
     [TestClassServiceFabricClient("http://localhost:9031")]
     //Non-Mandatory field: "testServerURL" represents server where test code will run
     public class DemoTestCases
     {
+
+        public DemoTestCases()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        // Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext) {; }
+
+        // Use ClassCleanup to run code after all tests in a class have run
+        [ClassCleanup()]
+        public static void MyClassCleanup() {; }
+
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            string currRunningTestMethod = this.TestContext.TestName;
+            //use 'currRunningTestMethod' to run test method specific initialization code before a test method run.
+        }
+
+        // Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            string currRunningTestMethod = this.TestContext.TestName;
+            //use 'currRunningTestMethod' to run test method specific cleanup code after a test method run.
+
+        }
+
+        #region test_cases
+
         [TestMethodServiceFabricClient(3)]
         //Non-Mandatory field: "maxRetriesToFetchTestResult" this represents the maximum number of retries Service Fabric test client should make to fetch 'TestResult' of a test case. We will only make next retry if earlier retry has failed. Default value is 2.
         public void TestMethodPass()
         {
-            //Every test method will be invoked before submitting test run request for respective server test case. If this code raises any exception including assertion failure exception, test run on server will not be done. And, the reported 'TestResult' will contain the exception raised during execution of this code and 'TestResult' would be reported as 'Failed' or any other status except "passed'.
-            //write your test code here
-            //you are responsible for :
-            //-> Writing a test case that does not runs indefinitely i.e. test case should end.
-            //-> One can write client side preTestCase code here. Like deploying a service etc.
-            int i = 0;
-            i++;
-            Assert.AreEqual(i, 1);
+            //->Only functions having 'TestMethodServiceFabricClient' attribute inside class having 'TestClassServiceFabricClient' attribute will run respective test case on server. Any other scheme of assigning attributes will not run test cases on server side.
+            //Functions having 'TestMethodServiceFabricClient' attribute will not run. Rather, their corresponding method(  Class name, namespace name and function name combination) on server side will run. So, they can be empty. However, 'TestInitialize' and 'TestCleanup' will run before and after it. So, flow is: execute 'TestInitialize' -> execute corresponding server side test code -> execute 'TestCleanup'.
+            ;
         }
 
         [TestMethodServiceFabricClient(2)]
         public void TestMethodFail()
         {
-            int i = 0;
-            Thread.Sleep(1000);
-            //due to assertion failure or throwing exception these tests are failing on client side itself. So, server side tests are not being run and client side run results are being considered. As, client side example test cases and server side example test cases have similar code. Commenting lines below in this function will not fail tests here, and allow tests to run on server side. In that case server side test case result will be considered. Please comment lines below to demo that scenario.
-            throw new Exception("User defined exception");//comment me as mentioned above to demo different scenario.
-            Assert.AreEqual(i, 1);//comment me as mentioned above to demo different scenario.
+            ;
         }
 
         [TestMethodServiceFabricClient(5)]
         public void TestMethodFailGetResultAfterThreeRetries()
         {
-            int i = 0;
-            //due to assertion failure or throwing exception these tests are failing on client side itself. So, server side tests are not being run and client side run results are being considered. As, client side example test cases and server side example test cases have similar code. Commenting lines below in this function will not fail tests here, and allow tests to run on server side. In that case server side test case result will be considered. Please comment lines below to demo that scenario.
-            Assert.AreEqual(i, 1);//comment me as mentioned above to demo different scenario.
+            ;
         }
 
         [TestMethodServiceFabricClient(1)]
         public void DemoTestMethodFailAgain()
         {
-            int i = 0;
-            //due to assertion failure or throwing exception these tests are failing on client side itself. So, server side tests are not being run and client side run results are being considered. As, client side example test cases and server side example test cases have similar code. Commenting lines below in this function will not fail tests here, and allow tests to run on server side. In that case server side test case result will be considered. Please comment lines below to demo that scenario.
-            Assert.AreEqual(i, 1);//comment me as mentioned above to demo different scenario.
+            ;
         }
 
         [TestMethodServiceFabricClient()]
@@ -67,5 +107,7 @@ namespace DemoTestCasesNamespace
         {
             ;
         }
+
+        #endregion
     }
 }
